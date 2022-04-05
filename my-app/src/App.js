ef0688e300a6c526a7ad4792ed36d9e466abc348
JongHoonSon <roka1684@gmail.com>
2022-04-05 17:55:58 +0900

    #7.2 Coin Tracker
    코드챌린지
    사용자가 보유한 달러를 input 으로 받고 해당 input을 money 라는 상태 값으로 관리한 후
    해당 money로 구매할 수 있는 coin의 갯수를 보여줌

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  const [money, setMoney] = useState(0);
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const onChangeMoney = (event) => {
+    setMoney(event.target.value);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((Response) => Response.json())
@@ -13,14 +17,22 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <h1>How much do you have?</h1>
+      <input value={money} onChange={onChangeMoney} type="number"></input> $
+      <h1>
+        You can buy this amount of coins! {loading ? "" : `(${coins.length})`}
+      </h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
         <ul>
           {coins.map((coin) => (
             <li key={coin.id}>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              {coin.name} ({coin.symbol}) :
+              {money === 0
+                ? " 0"
+                : ` ${(money / coin.quotes.USD.price).toFixed(5)}`}{" "}
+              coins
             </li>
           ))}
         </ul>
