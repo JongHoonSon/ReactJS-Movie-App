a6d57b78298d55dfa419d073344d0b7c8426139b
JongHoonSon <roka1684@gmail.com>
2022-04-05 14:45:23 +0900

    #5.1 Tour of CRA
    CSS 를 이용하는 방식2 (module.css 사용)
    
    module.css에 css를 작성하고, js에서 해당 css를 import하는 방식으로 사용함
    js에서 import되면 해당 css에 정의된 스타일 (.btn의 스타일 등)은
    styles 라는 js object 형태의 속성으로 사용할 수 있다. ( ex) styles.btn )
    
    각 컴포넌트 별로 필요한 css를 module.css 로 따로 만들어서 사용하면
    각 모듈에서 같은 클래스 이름을 사용하더라도 본인의 module.css에 정의된 css가 적용됨.
    
    이는 react.js가 모든 클래스 이름 뒤에 랜덤한 아이디를 정해주기 때문임
    ( ex) App.js에서도 .big 클래스를 쓰고 싶고, Button.js에서도 .big 클래스를 쓰고 싶다면 그냥 써도 됨,
    해당 .big 클래스의 css를 각각의 module.css 에서 작성하고 앱을 실행하면
    App 컴포넌트와 Button 컴포넌트의 클래스 뒤에 랜덤 id가 붙어서 구분할 수 있음 )
    ( ex) App.js는 className="big__abcd"
    Button.js는 className="big__eodq" 처럼 클래스 뒤에 붙는 랜덤 id로 서로 구분됨
    따라서 css가 덮어씨워지지 않음)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1 className={styles.btn}>Welcome back!</h1>
+      <Button text={"Continue"} />
       <Button text={"Continue"} />
     </div>
   );
