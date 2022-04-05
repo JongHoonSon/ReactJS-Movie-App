90f721c89d11ee2ddd55c834c015706a272b5a59
JongHoonSon <roka1684@gmail.com>
2022-04-05 16:36:34 +0900

    #7.0 To Do List part One
    초기상태로 reset

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,7 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created :)"); // 한 번 실행
-    return () => console.log("destroyed :("); // Hello 컴포넌트가 사라질 때 사용
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-    <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
