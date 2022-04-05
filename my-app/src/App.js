ed618eb8db5172e5c0624ea28bd0a60fd6c8856f
JongHoonSon <roka1684@gmail.com>
2022-04-05 16:54:33 +0900

    #7.0 To Do List part One
    새로운 toDo가 입력되면 해당 toDo 값을 toDos에 추가하는 setToDos 작성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const onChange = (event) => setTodo(event.target.value);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     console.log(toDo);
     if (toDo === "") {
       return;
     }
-    //동작
-    setTodo("");
+    setToDos((currentArray) => [toDo, ...currentArray]); // ...currentArray는 기존에 있던 값
+    // 상태 값이 Array인 것을 상태 관리할 때는 push 또는 = [] 처럼
+    // 직접적으로 값을 넣지 않고,
+    // 해당 Array를 새로운 Array로 변경한다.
+    setToDo("");
   };
+  console.log(toDos);
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
