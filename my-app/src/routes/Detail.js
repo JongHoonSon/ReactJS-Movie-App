1ba1716dc9e7ca9bc4fe3db1e2a59602f7dc291f
JongHoonSon <roka1684@gmail.com>
2022-04-05 23:33:34 +0900

    Detail.js는 파라미터로 받은 id로 영화 정보를 fetch해 가져와서 MovieDetail에 전달한다.

--- my-app/src/routes/Detail.js
+++ my-app/src/routes/Detail.js
@@ -1,19 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import MovieDetail from "../components/MovieDetail";
+import styles from "./Detail.module.css";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  console.log("id : ", id);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    console.log(json.data.movie);
+    console.log(json.data.movie.genres);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   console.log(id);
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div className={styles.floater}>
+      <div className={styles.box}>
+        {loading ? (
+          <div className={styles.loader}>
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <MovieDetail
+            title={movie.title}
+            rating={movie.rating}
+            cover={movie.medium_cover_image}
+            year={movie.year}
+            desc={movie.description_full}
+            genres={movie.genres}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
